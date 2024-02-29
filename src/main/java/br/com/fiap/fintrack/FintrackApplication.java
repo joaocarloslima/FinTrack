e1795eba6b293d8ef1909f114f9c408faa55cfb4
joaocarloslima <joaocarloslima@me.com>
2024-02-29 08:53:28 -0300

    criação do projet spring

---
+++ src/main/java/br/com/fiap/fintrack/FintrackApplication.java
@@ -0,0 +1,13 @@
+package br.com.fiap.fintrack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FintrackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FintrackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/fiap/fintrack/FintrackApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.fiap.fintrack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FintrackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
