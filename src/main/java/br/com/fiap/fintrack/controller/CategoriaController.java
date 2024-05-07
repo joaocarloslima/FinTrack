c02ae59b4f2c11b0a436dc2793592f7e76c97b05
joaocarloslima <joaocarloslima@me.com>
2024-05-07 11:51:59 -0300

    cache e documentação

--- src/main/java/br/com/fiap/fintrack/controller/CategoriaController.java
+++ src/main/java/br/com/fiap/fintrack/controller/CategoriaController.java
@@ -7,6 +7,9 @@ import static org.springframework.http.HttpStatus.NO_CONTENT;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.CacheConfig;
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -27,20 +30,21 @@ import lombok.extern.slf4j.Slf4j;
 @RestController
 @RequestMapping("categoria")
 @Slf4j
+@CacheConfig(cacheNames = "categorias")
 public class CategoriaController {
 
     @Autowired
     CategoriaRepository repository;
 
-
-
     @GetMapping
+    @Cacheable
     public List<Categoria> index() {
         return repository.findAll();
     }
 
     @PostMapping
     @ResponseStatus(CREATED)
+    @CacheEvict(allEntries = true)
     public Categoria create(@RequestBody @Valid Categoria categoria) { // injeta + binding
         log.info("cadastrando categoria {}", categoria);
         return repository.save(categoria);
@@ -59,6 +63,7 @@ public class CategoriaController {
 
     @DeleteMapping("{id}")
     @ResponseStatus(NO_CONTENT)
+    @CacheEvict(allEntries = true)
     public void destroy(@PathVariable Long id) {
         log.info("apagando categoria {}", id);
 
@@ -67,6 +72,7 @@ public class CategoriaController {
     }
 
     @PutMapping("{id}")
+    @CacheEvict(allEntries = true)
     public Categoria update( @PathVariable Long id, @RequestBody @Valid Categoria categoria) {
         log.info("atualizando categoria {} para {}", id, categoria);
 
